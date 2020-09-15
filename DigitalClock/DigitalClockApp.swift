d3f3e411ec05e81af512a2d18cb5a5b6cbb6e921
ASUI <asui@allswiftui.com>
2020-09-15 10:58:54 -0300

    Initial Commit

---
+++ DigitalClock/DigitalClockApp.swift
@@ -0,0 +1,18 @@
+// -----------------------------------------------------------------------------
+// File: DigitalClockApp.swift
+// Package: DigitalClock
+//
+// Created by: ALLSWIFTUI on 15-09-20
+// Copyright © 2020 allSwiftUI · https://allswiftui.com · @allswiftui
+// -----------------------------------------------------------------------------
+
+import SwiftUI
+
+@main
+struct DigitalClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
